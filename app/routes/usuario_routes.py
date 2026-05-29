8628e9a06014aac94dba221057c3193fef030d7a
Luana Monteiro <luanaljm36@gmail.com>
2026-05-29 16:55:11 -0300

    Update usuario_routes.py

--- app/routes/usuario_routes.py
+++ app/routes/usuario_routes.py
@@ -5,7 +5,6 @@ from app.models.usuario import Usuario
 from app.schemas.usuario_schema import UsuarioCreate, UsuarioResponse
 from passlib.context import CryptContext
 
-# Configuração da criptografia de senha
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 router = APIRouter(prefix="/usuarios", tags=["Usuários e LGPD"])
@@ -13,12 +12,10 @@ router = APIRouter(prefix="/usuarios", tags=["Usuários e LGPD"])
 
 @router.post("/", response_model=UsuarioResponse)
 def cadastrar_usuario(user: UsuarioCreate, db: Session = Depends(get_db)):
-    # Verifica se o e-mail já existe
     db_user = db.query(Usuario).filter(Usuario.email == user.email).first()
     if db_user:
         raise HTTPException(status_code=400, detail="E-mail já cadastrado")
 
-    # Transforma a senha em HASH (Requisito de Segurança)
     hashed_password = pwd_context.hash(user.senha)
 
     novo_usuario = Usuario(
