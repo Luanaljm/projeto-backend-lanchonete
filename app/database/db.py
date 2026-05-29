a2d9170caaf1feed4acc55cb36be0d9dd4837664
Luana Monteiro <luanaljm36@gmail.com>
2026-05-29 16:53:19 -0300

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Nome do arquivo de banco de dados que será criado automaticamente
 SQLALCHEMY_DATABASE_URL = "sqlite:///./lanchonete.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
