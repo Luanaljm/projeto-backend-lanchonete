38d815138da5d066e195bd898d6b6251fb10dbfe
Luana Monteiro <luanaljm36@gmail.com>
2026-05-29 16:52:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
 from app.database.db import engine, Base
-from app.models import pedido, usuario # Adicione o usuario aqui
-from app.routes import pedido_routes, usuario_routes # Adicione o usuario_routes
+from app.models import pedido, usuario 
+from app.routes import pedido_routes, usuario_routes 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="API Lanchonete Multicanal")
 
 app.include_router(pedido_routes.router)
-app.include_router(usuario_routes.router) # Registra a rota
+app.include_router(usuario_routes.router) 
 
 @app.get("/")
 def home():
